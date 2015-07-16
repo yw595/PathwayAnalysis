eba7c649f250c74ab61790a813aa52f8baf21452
Yiping Wang <yw595@cornell.edu>
2015-07-16 14:08:43 -0400

    functionalized biomaRtCommands.R

--- biomaRtCommands.R
+++ biomaRtCommands.R
@@ -3,6 +3,8 @@ library('gplots')
 library('lattice')
 library('biomaRt')
 
+convertEnsemblToEntrez = function (ensemblListFile, ensembltoEntrezFile)
+{
 mitocarta = read.table("MitoCarta Mouse Ensembl List.txt")
 ensemblHuman = useMart('ensembl', dataset='hsapiens_gene_ensembl')
 ensemblMouse = useMart('ensembl', dataset='mmusculus_gene_ensembl')
@@ -11,7 +13,12 @@ mapmitocarta1 = getBM(attributes = c('ensembl_gene_id','hsapiens_homolog_ensembl
 mapmitocarta2 = getBM(attributes = c('ensembl_gene_id','entrezgene'), filters = c('ensembl_gene_id','with_entrezgene'), values = list( as.vector(mapmitocarta1[,2]),TRUE ), mart = ensemblHuman)
 
 matchIdxs1 = match(intersect( as.vector(mapmitocarta1[,2]),as.vector(mapmitocarta2[,1]) ),mapmitocarta1[,2])
-matchIdxs2 = match(intersect( as.vector(mapmitocarta1[,2]),as.vector(mapmitocarta2[,1]) ),mapmitocarta1[,2])
+matchIdxs2 = match(intersect( as.vector(mapmitocarta1[,2]),as.vector(mapmitocarta2[,1]) ),mapmitocarta2[,1])
 
 mapmitocarta3 = cbind(mapmitocarta1[matchIdxs1,1], mapmitocarta2[matchIdxs2,2])
-write.table(mapmitocarta3, file="MitoCarta Mouse Ensembl To Human Entrez.csv", quote=FALSE, sep=",", row.names=FALSE)
+write.table(mapmitocarta3, file=ensemblToEntrezFile, quote=FALSE, sep=",", row.names=FALSE)
+}
+
+convertEnsemblToEntrez("MitoCarta Mouse Ensembl List.txt","MitoCarta Mouse Ensembl To Human Entrez.csv")
+convertEnsemblToEntrez("MitoMiner Mouse Ensembl List.txt","MitoMiner Mouse Ensembl To Human Entrez.csv")
+convertEnsemblToEntrez("All Mouse Ensembl List.txt","All Mouse Ensembl To Human Entrez.csv")
